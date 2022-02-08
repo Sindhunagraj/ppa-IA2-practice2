7176799aec51fc43bfd3e2f0023a9f3544510ae4
Sindhu03 <96463802+Sindhunagraj@users.noreply.github.com>
2022-02-08 07:19:05 +0000

    sindh8u

---
+++ p2original.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int input_sid()
+{
+  int a;
+  printf(" enter any number :");
+  scanf("%d",&n);
+  return a;
+  }
+  int check_ scalen(int a,int b,int c)
+  }
+  int i;
+  f
+  for(i=2;i</2;++i)
+  { 
+    if(n%==0)
+  return 0;
+  eles
+  printf("COMPOSIT");
+  }  
+  int main()
+  {
+    int n,x;
+    n=input_number();
+    x=is_composite(n)
+    output(n,x);
+    return 0
+  }
---
+++ p3original.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int input_number()
+{
+  int l;
+  printf("Enter side length ");
+  scanf ("%d", &l);
+  return l;
+}
+
+int is _composit (int n)
+{ 
+  int p=0;
+  for (int i=1,i<=n,i++) 
+}
